d3a49aea2747139bb34d18754b1f795934b9252b
himediajrjin <himediajr.jin@gmail.com>
2023-05-16 16:20:24 +0900

    Mybatis

--- springProject/intellij/DeptSpring/src/test/java/com/hi/deptspring/ConnectionTest.java
+++ springProject/intellij/DeptSpring/src/test/java/com/hi/deptspring/ConnectionTest.java
@@ -1,5 +1,9 @@
 package com.hi.deptspring;
 
+import com.hi.deptspring.deptspring.domain.DeptDTO;
+import com.hi.deptspring.deptspring.mapper.DeptMapper;
+import com.hi.deptspring.deptspring.mapper.TimeMapper;
+import com.hi.deptspring.deptspring.mapper.TimeMapper2;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -11,6 +15,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 @Log4j2
 @ExtendWith(SpringExtension.class)
@@ -20,6 +25,47 @@ public class ConnectionTest {
 
     @Autowired
     private DataSource dataSource;
+
+
+    // Service 클래스
+    @Autowired(required = false)
+    private TimeMapper timeMapper;
+
+    @Autowired(required = false)
+    private TimeMapper2 timeMapper2;
+
+    @Autowired(required = false)
+    private DeptMapper deptMapper;
+
+
+    @Test
+    public void getDeptListTest(){
+        List<DeptDTO> list = deptMapper.selectAll();
+        log.info(">>>>>  " + list);
+
+        log.info(deptMapper.selectByDeptno(10));
+        log.info(deptMapper.selectByDeptno(20));
+        log.info(deptMapper.selectByDeptno(30));
+        log.info(deptMapper.selectByDeptno(40));
+        log.info(deptMapper.selectByDeptno(50));
+
+
+    }
+
+
+
+
+    @Test
+    public void getTimeMapperTest(){
+        
+        // TimeMapper 구현체의 주입영부 , 메소드 실행 결과
+
+        log.info(timeMapper.getTime());
+        log.info(timeMapper2.getTime());
+
+    }
+
+
 
     @Test
     public void connectionTest() throws SQLException {
