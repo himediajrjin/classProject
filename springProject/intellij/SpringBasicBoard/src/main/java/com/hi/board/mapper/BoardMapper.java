0785eb4f7392b8e8c8d65451ebcaf0ee797b5bf0
himediajrjin <himediajr.jin@gmail.com>
2023-05-19 18:22:28 +0900

    Spring Board

--- springProject/intellij/SpringBasicBoard/src/main/java/com/hi/board/mapper/BoardMapper.java
+++ springProject/intellij/SpringBasicBoard/src/main/java/com/hi/board/mapper/BoardMapper.java
@@ -4,6 +4,7 @@ import com.hi.board.domain.BoardDTO;
 import com.hi.board.domain.RequestModifyRequest;
 import com.hi.board.domain.RequestRegBoard;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -11,6 +12,10 @@ import java.util.List;
 public interface BoardMapper {
 
 
+    List<BoardDTO> selectList(int startNum, int count);
+    // selectList(0, 5)   #{param1} => 0, #{param2} => 5
+
+    int selectTotalCount();
 
     List<BoardDTO> selectAll();
 
