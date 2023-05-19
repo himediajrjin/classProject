46f3c5723fc80cd822cd0f216ecc6d7e6f5027c8
himediajrjin <himediajr.jin@gmail.com>
2023-05-19 16:21:17 +0900

    Spring Board

--- springProject/intellij/SpringBasicBoard/src/main/java/com/hi/board/service/BoardDeleteService.java
+++ springProject/intellij/SpringBasicBoard/src/main/java/com/hi/board/service/BoardDeleteService.java
@@ -1,8 +1,12 @@
 package com.hi.board.service;
 
+import com.hi.board.domain.BoardDTO;
 import com.hi.board.mapper.BoardMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 
 @Service
 public class BoardDeleteService {
@@ -10,7 +14,27 @@ public class BoardDeleteService {
     @Autowired
     private BoardMapper boardMapper;
 
-    public int deleteBoard(int bno){
+    public int deleteBoard(int bno, HttpServletRequest request){
+
+        // 삭제 대상의 파일 이름을 획득
+        BoardDTO board = boardMapper.selectByBno(bno);
+        String delFileName = board.getFile();
+        
+        // DB 삭제
+        int result = boardMapper.deleteBoard(bno);
+
+        // 파일 삭제
+        if(delFileName != null){
+            // 웹경로
+            String uri = "/uploadfile/board";
+            // 실제경로
+            String realPath = request.getSession().getServletContext().getRealPath(uri);
+            // 삭제 대상파일의 => File 객체
+            File delFile = new File(realPath, delFileName);
+            // 현재 File 이 존재 여부 확인
+            // 존재하면 삭제
+        }
+
         return boardMapper.deleteBoard(bno);
     }
 
