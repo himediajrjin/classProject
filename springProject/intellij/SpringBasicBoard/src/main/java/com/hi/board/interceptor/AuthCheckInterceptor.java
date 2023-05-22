0449d77eea9bf1812a40ca866535786d7ceaaf4e
himediajrjin <himediajr.jin@gmail.com>
2023-05-22 17:46:38 +0900

    Spring Board

--- springProject/intellij/SpringBasicBoard/src/main/java/com/hi/board/interceptor/AuthCheckInterceptor.java
+++ springProject/intellij/SpringBasicBoard/src/main/java/com/hi/board/interceptor/AuthCheckInterceptor.java
@@ -1,5 +1,6 @@
 package com.hi.board.interceptor;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -7,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@Log4j2
 public class AuthCheckInterceptor implements HandlerInterceptor {
 
     @Override
@@ -15,18 +17,23 @@ public class AuthCheckInterceptor implements HandlerInterceptor {
             HttpServletResponse response,
             Object handler) throws Exception {
 
-        // 세션의 속성에서 "loginInfo" 속성 존재 유무에 따라
+
         HttpSession session = request.getSession();
 
-        // 있다면 return true
+        // 세션의 속성에서 "loginInfo" 속성 존재 유무에 따라
+        if(session.getAttribute("loginInfo") != null){
+            log.info("회원 로그인 체크 : 로그인 상태");
+            // 있다면 return true
+            return true;
+        }
 
-        
-        
         // 없다면 /login 리디렉션 처리
+        log.info("회원 로그인 체크 : 비 로그인 상태");
 
-
+        response.sendRedirect("/login");
 
         return false;
+
     }
 
     @Override
