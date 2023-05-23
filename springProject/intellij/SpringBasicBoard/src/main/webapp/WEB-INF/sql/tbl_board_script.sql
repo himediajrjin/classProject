b62d7db36f7d507420f4348324f5eb132a82ccb9
himediajrjin <himediajr.jin@gmail.com>
2023-05-23 10:22:38 +0900

    Spring Board

--- springProject/intellij/SpringBasicBoard/src/main/webapp/WEB-INF/sql/tbl_board_script.sql
+++ springProject/intellij/SpringBasicBoard/src/main/webapp/WEB-INF/sql/tbl_board_script.sql
@@ -12,6 +12,15 @@ values ('게시글 테스트1', '게시글 쓰기 테스트 1', '테스터');
 
 -- insert into tbl_board (title, content, writer) values (#{}, #{}, #{})
 
+insert into tbl_board (title, content, memidx)
+value ('게시글 테스트1', '글쓰기 테스트 1', 1);
+
+
+
+insert into tbl_board (title, content, memidx)
+value ('게시글 테스트1', '글쓰기 테스트 1', 5);
+
+
 
 
 -- update
@@ -23,3 +32,16 @@ where bno=8;
 
 
 -- delete
+
+
+
+
+-- select 구간
+SELECT * FROM project.tbl_board order by bno desc limit 0,5; -- 1page (1-1)*5 => 0
+SELECT * FROM project.tbl_board order by bno desc limit 5,5; -- 2page (2-1)*5 => 5
+SELECT * FROM project.tbl_board order by bno desc limit 10,5; -- 3page (3-1)*5 => 10
+SELECT * FROM project.tbl_board order by bno desc limit 15,5; -- 4page (4-1)*5 => 15
+
+select count(*) from tbl_board;
+
+SELECT * FROM project.tbl_board order by bno desc;
