0887e4a2cbac013798190f07f9c13a6a4c93deef
himediajrjin <himediajr.jin@gmail.com>
2023-05-23 11:20:14 +0900

    Spring Board

--- springProject/intellij/SpringBasicBoard/src/main/webapp/WEB-INF/sql/tbl_board_script.sql
+++ springProject/intellij/SpringBasicBoard/src/main/webapp/WEB-INF/sql/tbl_board_script.sql
@@ -6,6 +6,12 @@
 select * from tbl_board;
 
 
+
+
+
+
+
+
 -- insert
 insert into tbl_board (title, content, writer) 
 values ('게시글 테스트1', '게시글 쓰기 테스트 1', '테스터');
@@ -15,7 +21,7 @@ values ('게시글 테스트1', '게시글 쓰기 테스트 1', '테스터');
 insert into tbl_board (title, content, memidx)
 value ('게시글 테스트1', '글쓰기 테스트 1', 1);
 
-
+-- insert into tbl_board (title, content, memidx) value (#{}, #{}, #{})
 
 insert into tbl_board (title, content, memidx)
 value ('게시글 테스트1', '글쓰기 테스트 1', 5);
