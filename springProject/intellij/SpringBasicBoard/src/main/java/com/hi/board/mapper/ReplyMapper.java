7498c5db453815733de62013bd812fe866affbd0
himediajrjin <himediajr.jin@gmail.com>
2023-05-31 18:24:19 +0900

    BasicBoard reply

--- springProject/intellij/SpringBasicBoard/src/main/java/com/hi/board/mapper/ReplyMapper.java
+++ springProject/intellij/SpringBasicBoard/src/main/java/com/hi/board/mapper/ReplyMapper.java
@@ -4,9 +4,15 @@ import com.hi.board.domain.ReplyDTO;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface ReplyMapper {
+
+
+    @Select("select * from tbl_reply where rno=#{rno}")
+    ReplyDTO selectByRno(int rno);
+
 
     // insert
     @Insert("insert into tbl_reply (bno, reply, memIdx) values (#{bno},#{reply},#{memIdx})")
